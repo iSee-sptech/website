ae11b13754762f59adb78772eb6000be3fa99b2e
Andreas Rodrigues <andreas.bittencourt@sptech.school>
2022-09-20 08:32:28 -0300

    Mascara na index

---
+++ public/js/mascarasIndex.js
@@ -0,0 +1,13 @@
+const inputTelPerfil = document.getElementById("contactTelefone");
+
+inputTelContact.addEventListener("keypress", () => {
+  let inputTelContact = contactTelefone.value.length;
+
+  if (inputTelContact === 0) {
+    contactTelefone.value += "(";
+  } else if (inputTelContact === 3) {
+    contactTelefone.value += ") ";
+  } else if (inputTelContact === 10) {
+    contactTelefone.value += "-";
+  }
+});
