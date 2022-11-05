bf8120000eb0fcc01a08ad453f30b379d6a90508
Gabriel Nogueira <gabriel.costa@sptech.school>
2022-11-04 21:53:05 -0300

    script tabelas azure

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -3,20 +3,19 @@ use iSee;
 
 create table Usuarios (
 idUsuario int primary key auto_increment,
-nomeUsuario varchar (100) not null,
-cargoUsuario varchar (30) not null,
-emailUsuario varchar (70) unique not null,
+nomeUsuario varchar (100),
+cargoUsuario varchar (30),
+emailUsuario varchar (70) unique,
 cepUsuario char (9),
-cpfUsuario char (14) unique,
-telefoneUsuario char (15) unique not null,
+cpfUsuario char (14) unique not null,
+telefoneUsuario char (15) unique,
 crmUsuario  char (6),
 senhaUsuario varchar (20) not null,
-dataNascUsuario date not null,
+dataNascUsuario date,
 imagemPerfilUsuario varchar (255),
 numeroLocalUsuario varchar (6),
 complementoLocalUsuario varchar (25)
 );
-select * from usuarios;
 
 create table Maquinas (
 idMaquina int primary key auto_increment,
@@ -26,36 +25,31 @@ fabricanteMaquina varchar(25),
 arquiteturaMaquina varchar(25),
 tempoDeAtividade long,
 nomeMaquina varchar (100),
-discoMaquina varchar(100),
-ramMaquina varchar(100),
-processadorMaquina varchar(100),
+discoMaquina varchar(10),
+ramMaquina varchar(10),
+processadorMaquina varchar(10),
 cepMaquina char (9) unique,
 imgMaquina varchar (255),
 complementoMaquina varchar (50),
-pontoReferenciaMaquina varchar (25),
-numeroMaquina int,
 fkUsuario int,
 foreign key (fkUsuario)
 references Usuarios (idUsuario)
 );
-select * from maquinas;
 
 create table Etiqueta (
 idEtiqueta int primary key auto_increment,
-qualidadeEtiqueta varchar (25),
-tempoSemProblema varchar (15),
-etiquetacol varchar (30),
 fkMaquina int,
+nomeEtiqueta varchar(50),
+datahoraEtiqueta datetime,
 foreign key (fkMaquina)
 references Maquinas (idMaquina)
 );
-select * from etiqueta;
 
 create table Historico (
 idHistorico int primary key auto_increment,
-usoRamHistorico long,
-usoProcessadorHistorico long,
-usoDiscoHistorico long,
+usoRamHistorico varchar(10),
+usoProcessadorHistorico varchar(10),
+usoDiscoHistorico varchar(10),
 dataHoraHistorico datetime default current_timestamp,
 statusHistorico varchar (50),
 logHistorico varchar (255),
@@ -63,7 +57,6 @@ fkMaquinaHistorico int,
 foreign key (fkMaquinaHistorico)
 references Maquinas (idMaquina)
 );
-select * from Historico;
 
 create table Lembrete(
 idLembrete int primary key auto_increment,
@@ -74,4 +67,14 @@ fkUsuario int,
 foreign key (fkUsuario)
 references Usuarios (idUsuario)
 );
-select * from Lembrete;
+
+create table Alerta (
+idAlerta int primary key auto_increment,
+fkMaquina int,
+componente varchar(10),
+nivelAlerta varchar(10),
+dado varchar(10),
+datahoraAlerta datetime,
+foreign key (fkMaquina)
+references Maquinas (idMaquina)
+);
