8f92854a384127c6082b054ed584c743598cc3da
Andreas Rodrigues <andreas.bittencourt@sptech.school>
2022-11-06 21:39:31 -0300

    Bottom-bar com todos os eventos de passar o mouse por cima

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,13 +1,10 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idMaquina, limite_linhas) {
     instrucaoSql = `select 
-                        lm35,
-                        data_horario,
-                        DATE_FORMAT(data_horario,'%H:%i:%s') as momento_grafico
-                    from hist_medicao
-                    where fk_sensor = ${idAquario}
-                    order by data_horario desc limit ${limite_linhas}`;
+                    usoRamHistorico,
+                    from Historico
+                    where idMaquina = ${idMaquina}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
