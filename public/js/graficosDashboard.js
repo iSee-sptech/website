05849185534a739c2622d7747e17dbb37449ec86
Andreas Rodrigues <andreas.bittencourt@sptech.school>
2022-11-15 18:47:00 -0300

    Dashboard quase finalizada, faltando apenas jogar na tela

--- public/js/graficosDashboard.js
+++ public/js/graficosDashboard.js
@@ -1,85 +1,75 @@
 // GRAFICOS DA ESQUEDA
-var data = [{
-    label: 'Uso de RAM',
-    data: [50, 80, 90, 30],
-    backgroundColor: [
-        '#7d2de2',
-        '#2DE23F',
-        '#2DB7E2',
-        '#E22D63'
-    ],
-}];
+var data = [
+  {
+    label: "Uso de RAM",
+    data: [10, 80, 90, 30],
+    backgroundColor: ["#7d2de2", "#2DE23F", "#2DB7E2", "#E22D63"],
+  },
+];
 
 var options01 = {
-    color: '#fff',
-    scales: {
-        x: {
-            ticks: {
-                color: "#fff",
-                font: {
-                    size: 10,
-                }
-            }
+  color: "#fff",
+  scales: {
+    x: {
+      ticks: {
+        color: "#fff",
+        font: {
+          size: 10,
         },
-        y: {
-            ticks: {
-                color: "#fff",
-                font: {
-                    size: 10,
-                }
-            }
-        }
+      },
     },
-
     y: {
-        color: '#fff',
-        beginAtZero: true,
-        max: 100,
-        ticks: {
-            maxTicksLimit: 10,
-        }
+      ticks: {
+        color: "#fff",
+        font: {
+          size: 10,
+        },
+      },
     },
+  },
+
+  y: {
+    color: "#fff",
+    beginAtZero: true,
+    max: 100,
+    ticks: {
+      maxTicksLimit: 10,
+    },
+  },
 };
 
-
-var ctx = document.getElementById("myBarChart").getContext('2d');
+var ctx = document.getElementById("myBarChart").getContext("2d");
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Caixa 1', 'Caixa 2', 'Caixa 3', 'Caixa 4'],
-        datasets: data
-    },
-    options: options01
+  type: "bar",
+  data: {
+    labels: ["Caixa 1", "Caixa 2", "Caixa 3", "Caixa 4"],
+    datasets: data,
+  },
+  options: options01,
 });
-
 
 // ^^^^^^^^^^^^^^
 
-
 // // GRAFICOS DA ESQUEDA
 
-var data02 = [{
-    label: 'Uso de RAM',
+var data02 = [
+  {
+    label: "Uso de RAM",
     data: [50, 80, 90, 30],
-    backgroundColor: [
-        '#7d2de2',
-        '#2DE23F',
-        '#2DB7E2',
-        '#E22D63'
-    ],
-    borderColor:false
-}];
+    backgroundColor: ["#7d2de2", "#2DE23F", "#2DB7E2", "#E22D63"],
+    borderColor: false,
+  },
+];
 
 var options02 = {
-    color: '#fff',
+  color: "#fff",
 };
 
-
-var ctx = document.getElementById("pieChart").getContext('2d');
+var ctx = document.getElementById("pieChart").getContext("2d");
 var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        datasets: data02
-    },
-    options: options02
+  type: "doughnut",
+  data: {
+    datasets: data02,
+  },
+  options: options02,
 });
--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,29 +1,28 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idMaquina, limite_linhas) {
-    instrucaoSql = `select 
+function buscarUltimasMedidas() {
+  instrucaoSql = `select 
+    usoRamHistorico as usoRam,
+    dataHoraHistorico,
+    DATE_FORMAT(dataHoraHistorico,'%H:%i:%s') as momento_grafico
+    from historico
+    order by usoRamHistorico desc limit 10`;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal() {
+  instrucaoSql = `select 
                     usoRamHistorico,
-                    from Historico
-                    where idMaquina = ${idMaquina}`;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+                        dataHoraHistorico, 
+                        DATE_FORMAT(dataHoraHistorico,'%H:%i:%s') as momento_grafico
+                        from historico order by dataHoraHistorico desc limit 1`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
-
-function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
-                        lm35,
-                        data_horario, 
-                        DATE_FORMAT(data_horario,'%H:%i:%s') as momento_grafico, 
-                        fk_sensor 
-                        from hist_medicao where fk_sensor = ${idAquario} 
-                    order by data_horario desc limit 1`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
-}
+  buscarUltimasMedidas,
+  buscarMedidasEmTempoReal,
+};
